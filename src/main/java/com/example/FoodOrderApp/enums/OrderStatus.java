a14fd09894c9a1d439052a9e5d113b77e69676cd
factor-4 <snk.sojib@gmail.com>
2025-09-24 01:07:19 +0300

    enum added

---
+++ src/main/java/com/example/FoodOrderApp/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.example.FoodOrderApp.enums;
+
+public enum OrderStatus {
+    INITIALIZED,
+    CONFIRMED,
+    ON_THE_WAY,
+    DELIVERED,
+    CANCELLED,
+    FAILED
+}
---
+++ src/main/java/com/example/FoodOrderApp/enums/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.example.FoodOrderApp.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED,
+    REFUNDED
+}
