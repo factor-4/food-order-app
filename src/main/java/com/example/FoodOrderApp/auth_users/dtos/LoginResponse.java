3d27a895c1b8e2d0bd2a6f2a29ce746a6eb2c7e0
factor-4 <snk.sojib@gmail.com>
2025-09-24 23:16:02 +0300

    user entity-dto-repository

---
+++ src/main/java/com/example/FoodOrderApp/auth_users/dtos/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.example.FoodOrderApp.auth_users.dtos;
+
+import java.util.List;
+
+public class LoginResponse {
+
+    private String token;
+    private List<String> roles;
+
+
+
+}
---
+++ src/main/java/com/example/FoodOrderApp/auth_users/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.FoodOrderApp.auth_users.repository;
+
+import com.example.FoodOrderApp.auth_users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional <User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
