fd6721d650b12e71e1a4daa0f991019d0b98b7b8
factor-4 <snk.sojib@gmail.com>
2025-09-25 14:35:33 +0300

    category repo, dto, entity

---
+++ src/main/java/com/example/FoodOrderApp/category/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.FoodOrderApp.category.repository;
+
+import com.example.FoodOrderApp.category.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    
+}
