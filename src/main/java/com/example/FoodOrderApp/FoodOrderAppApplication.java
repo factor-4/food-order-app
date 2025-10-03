dea34618149e65adb85c42ab288ff2315727fa45
factor-4 <snk.sojib@gmail.com>
2025-10-03 12:01:20 +0300

    email sending testing

--- src/main/java/com/example/FoodOrderApp/FoodOrderAppApplication.java
+++ src/main/java/com/example/FoodOrderApp/FoodOrderAppApplication.java
@@ -1,13 +1,39 @@
 package com.example.FoodOrderApp;
 
+import com.example.FoodOrderApp.email_notification.dtos.NotificationDTO;
+import com.example.FoodOrderApp.email_notification.services.NotificationService;
+import com.example.FoodOrderApp.enums.NotificationType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+//@RequiredArgsConstructor
 public class FoodOrderAppApplication {
+//    private final NotificationService notificationService;
 
 	public static void main(String[] args) {
-		SpringApplication.run(FoodOrderAppApplication.class, args);
+
+        SpringApplication.run(FoodOrderAppApplication.class, args
+        );
 	}
+
+//    @Bean
+//    CommandLineRunner runner(){
+//        return args -> {
+//            NotificationDTO notificationDTO = NotificationDTO.builder()
+//                    .recipient("khansshahn@gmail.com")
+//                    .subject("Email sending test")
+//                    .body("Hi, how you doing!")
+//                    .type(NotificationType.EMAIL)
+//                    .build();
+//            notificationService.sendEmail(notificationDTO);
+//
+//        };
+//    }
 
 }
