90956f1f0e1770a47bdca45bb3090408455f88fe
factor-4 <snk.sojib@gmail.com>
2025-10-05 16:00:11 +0300

    role service added

---
+++ src/main/java/com/example/FoodOrderApp/role/services/RoleService.java
@@ -0,0 +1,18 @@
+package com.example.FoodOrderApp.role.services;
+
+import com.example.FoodOrderApp.response.Response;
+import com.example.FoodOrderApp.role.dtos.RoleDTO;
+
+import java.util.List;
+
+public interface RoleService  {
+
+    Response<RoleDTO> createRole(RoleDTO roleDTO);
+
+    Response<RoleDTO> updateRole(RoleDTO roleDTO);
+
+    Response<List <RoleDTO> > getAllROle(RoleDTO roleDTO);
+
+    Response<?> deleteROle(Long id);
+
+}
---
+++ src/main/java/com/example/FoodOrderApp/role/services/RoleServiceImpl.java
@@ -0,0 +1,81 @@
+package com.example.FoodOrderApp.role.services;
+
+import com.example.FoodOrderApp.exceptions.BadRequestException;
+import com.example.FoodOrderApp.exceptions.NotFoundException;
+import com.example.FoodOrderApp.response.Response;
+import com.example.FoodOrderApp.role.dtos.RoleDTO;
+import com.example.FoodOrderApp.role.entity.Role;
+import com.example.FoodOrderApp.role.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RoleServiceImpl implements RoleService{
+
+    private final RoleRepository roleRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Response<RoleDTO> createRole(RoleDTO roleDTO) {
+        Role role = modelMapper.map(roleDTO, Role.class);
+
+        Role savedRole = roleRepository.save(role);
+
+
+        return Response.<RoleDTO>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Role created successfully")
+                .data(modelMapper.map(savedRole, RoleDTO.class))
+                .build();
+    }
+
+    @Override
+    public Response<RoleDTO> updateRole(RoleDTO roleDTO) {
+        Role existingRole = roleRepository.findById(roleDTO.getId())
+                .orElseThrow(()-> new NotFoundException("Role not found"));
+        if(roleRepository.findByName(roleDTO.getName()).isPresent())
+            throw new BadRequestException("Role with name already exists");
+
+        existingRole.setName(roleDTO.getName());
+        Role updateRole = roleRepository.save(existingRole);
+
+        return Response.<RoleDTO>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Role updated successfully")
+                .data(modelMapper.map(updateRole, RoleDTO.class))
+                .build();
+    }
+
+    @Override
+    public Response<List<RoleDTO>> getAllROle(RoleDTO roleDTO) {
+        List<Role> roles = roleRepository.findAll();
+        List<RoleDTO> roleDTOS = roles.stream()
+                .map(role-> modelMapper.map(role, RoleDTO.class))
+                .toList();
+
+        return Response.< List <RoleDTO>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Role retrieved successfully")
+                .data(roleDTOS)
+                .build();
+    }
+
+    @Override
+    public Response<?> deleteROle(Long id) {
+        if(!roleRepository.existsById(id)){
+           throw new NotFoundException("Role does not exists");
+        }
+        roleRepository.deleteById(id);
+        return Response.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Role deleted successfully")
+                .build();
+    }
+}
