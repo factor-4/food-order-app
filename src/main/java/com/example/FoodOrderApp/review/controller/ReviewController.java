380d62978aef798fb04d2239fe7889dab9d4ef04
factor-4 <snk.sojib@gmail.com>
2025-10-17 23:24:53 +0300

    review controller added

---
+++ src/main/java/com/example/FoodOrderApp/review/controller/ReviewController.java
@@ -0,0 +1,42 @@
+package com.example.FoodOrderApp.review.controller;
+
+
+import com.example.FoodOrderApp.response.Response;
+import com.example.FoodOrderApp.review.dtos.ReviewDTO;
+import com.example.FoodOrderApp.review.services.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<Response<ReviewDTO>> createReview(
+            @RequestBody @Valid ReviewDTO reviewDTO
+    ){
+        return ResponseEntity.ok(reviewService.createReview(reviewDTO));
+    }
+
+    @GetMapping("/menu-item/{menuId}")
+    public ResponseEntity<Response<List<ReviewDTO>>> getReviewsForMenu(
+            @PathVariable Long menuId) {
+        return ResponseEntity.ok(reviewService.getReviewsForMenu(menuId));
+    }
+
+    @GetMapping("/menu-item/average/{menuId}")
+    public ResponseEntity<Response<Double>> getAverageRating(
+            @PathVariable Long menuId) {
+        return ResponseEntity.ok(reviewService.getAverageRating(menuId));
+    }
+
+}
